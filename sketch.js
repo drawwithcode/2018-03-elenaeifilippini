6561f81c7d8183d9aab449219063fbd759c620e0
Elena <elenafilippi9e@gmail.com>
2018-10-09 23:33:37 +0200

    modifiche da tutorial

--- sketch.js
+++ sketch.js
@@ -1,11 +1,48 @@
 function preload(){
   // put preload code here
 }
+ var inc = 0.01;
+ var yoff=0;
 
 function setup() {
-  // put setup code here
+  createCanvas(800,800);
+  pixelDensity(1);
+
 }
 
 function draw() {
-  // put drawing code here
+
+background(200);
+var xoff = 0;
+
+
+
+
+
+
+loadPixels();
+for ( var x =0; x < width; x ++){
+
+    for( var y = 0; y < height; y ++) {
+
+      var index = (x + y * width) * 4;
+      var r= noise(xoff, yoff ) * 255;
+
+      pixels[ index + 0] = r;
+      pixels[ index + 1] = 0;
+      pixels[ index + 2] = r;
+      pixels[ index + 3] = 255;
+
+      xoff += mouseX/1000;
+    }
+yoff += 0.01;
+  }
+  updatePixels();
+
+
+textSize(30);
+text("prova a spostare l'antenna", width/2, height/2);
+
+
+
 }
